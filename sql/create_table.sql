b3885dc964e3816075562e16e2e735d987ccfd59
Aditto Al Ashir <72722721+aditto-ashir-40049@users.noreply.github.com>
2026-04-06 18:28:07 -0400

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,15 +1,36 @@
-CREATE TABLE dbo.yellow_taxi (
-    VendorID            INT,
-    pickup_datetime     DATETIME2,
-    dropoff_datetime    DATETIME2,
-    passenger_count     INT,
-    trip_distance       FLOAT,
-    RatecodeID          INT,
-    PULocationID        INT,
-    DOLocationID        INT,
-    payment_type        INT,
-    fare_amount         FLOAT,
-    tip_amount          FLOAT,
-    tolls_amount        FLOAT,
-    total_amount        FLOAT
-);
+CREATE VIEW dbo.yellow_taxi AS
+SELECT *, '2015-01' AS data_period
+FROM OPENROWSET(
+    BULK 'yellow_tripdata_2015-01.csv',
+    DATA_SOURCE = 'BlobStorage',
+    FORMAT = 'CSV',
+    PARSER_VERSION = '2.0',
+    HEADER_ROW = TRUE
+) AS taxi
+UNION ALL
+SELECT *, '2016-01' AS data_period
+FROM OPENROWSET(
+    BULK 'yellow_tripdata_2016-01.csv',
+    DATA_SOURCE = 'BlobStorage',
+    FORMAT = 'CSV',
+    PARSER_VERSION = '2.0',
+    HEADER_ROW = TRUE
+) AS taxi
+UNION ALL
+SELECT *, '2016-02' AS data_period
+FROM OPENROWSET(
+    BULK 'yellow_tripdata_2016-02.csv',
+    DATA_SOURCE = 'BlobStorage',
+    FORMAT = 'CSV',
+    PARSER_VERSION = '2.0',
+    HEADER_ROW = TRUE
+) AS taxi
+UNION ALL
+SELECT *, '2016-03' AS data_period
+FROM OPENROWSET(
+    BULK 'yellow_tripdata_2016-03.csv',
+    DATA_SOURCE = 'BlobStorage',
+    FORMAT = 'CSV',
+    PARSER_VERSION = '2.0',
+    HEADER_ROW = TRUE
+) AS taxi;
